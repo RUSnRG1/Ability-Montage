8b5276d62af7230554fdde3c8b5c9133f5d1d6bf
RUSnRG1 <rusnrg1@gmail.com>
2023-11-11 00:46:57 +0900

    2023/11/10 DESKTOP

---
+++ trial/loadImage.js
@@ -0,0 +1,64 @@
+// 各フォルダとその中の画像ファイル名のオブジェクト
+const imageFolders = {
+    '0': ['00.gif','01.gif','02.gif','03.gif','04.gif','05.gif'],
+    '1': ['00.gif','01.gif','02.gif','03.gif','04.gif'],
+    // ...他のフォルダ...
+};
+
+// 特定のフォルダの画像のみを読み込む関数
+function loadImageFromFolder(folderName) {
+    const gameImages = document.getElementById('gameImages');
+    gameImages.innerHTML = ''; // 既存の画像をクリア
+  
+    const images = imageFolders[folderName];
+    images.forEach(file => {
+      const img = document.createElement('img');
+      img.src = `images/${folderName}/${file}`;
+      img.alt = file;
+      gameImages.appendChild(img);
+    });
+}
+  
+function loadSpecificImages() {
+    const gameImages = document.getElementById('gameImages');
+    gameImages.innerHTML = ''; // 既存の画像をクリア
+  
+    // 特定の画像のリスト（フォルダ名とファイル名を含む）
+    const specificImages = [
+      { folder: '0', file: '05.gif' },
+      { folder: '0', file: '03.gif' },
+      { folder: '1', file: '04.gif' },
+      { folder: '1', file: '02.gif' },
+      { folder: '0', file: '00.gif' },
+      { folder: '1', file: '00.gif' },
+    ];
+  
+    specificImages.forEach(({ folder, file }) => {
+      const img = document.createElement('img');
+      img.src = `images/${folder}/${file}`;
+      img.alt = file;
+      gameImages.appendChild(img);
+    });
+}
+
+  // イベントリスナーのセットアップ関数にフォルダ選択ボタン用のリスナーを追加
+function setupListeners() {
+    document.getElementById('loadFolder0').addEventListener('click', function() {
+      loadImageFromFolder('0');
+    });
+  
+    document.getElementById('loadFolder1').addEventListener('click', function() {
+      loadImageFromFolder('1');
+    });
+
+    document.getElementById('loadSpecificImages').addEventListener('click', function() {
+        loadSpecificImages();
+      });
+  
+    // ...他のイベントリスナー...
+}
+  
+  // DOMContentLoadedイベントで関数を実行
+document.addEventListener('DOMContentLoaded', function() {
+    setupListeners();
+});
