5fba4a72aed9ab23dca8963161f7a8afb03d37ba
RUSnRG <rusnrg1@gmail.com>
2024-02-16 17:26:40 +0900

    At shikenkaijo

--- FlappyTotetsu/gameScreen.js
+++ FlappyTotetsu/gameScreen.js
@@ -1,4 +1,25 @@
 // 自機の状態
+// 定数
+const GRAVITY = 1.2; // 重力
+const JUMP_FORCE = -18; // ジャンプの力
+const FRAME_RATE = 60; // フレームレート
+
+const HAND_SIZE = 50;//障害物の横サイズ
+const INTERVAL = 550;//前後の障害物の間隔
+const MOVE_SPEED = 6;//動くスピード
+
+const PLAYER_R = 30;//プレイヤー円の半径
+
+const FOOD_R = 10;
+const FOOD_H = 400;
+
+let score = 0;
+
+let introFlag = false;
+let gameFlag = true;
+let resultFlag = false;
+
+
 const player = {
   x: 310,
   y: 300, // Y座標
@@ -6,56 +27,176 @@ const player = {
 };
 
 const rects = [];
-let n = 20;
+let n = 50;
 for (let i = 0; i < n; ++i){
   const width = 40;
   const height = 60 - 30 / n * i;
   const xr = 310 - 280 * (i + 1) / n;
   const yr = 300;
   const angle = 0;
+  const jumpFlag = 1;
   rects.push({ xr, yr, width, height, angle });
 }
+
+const hands = [];
+let n2 = 6;
+for (let i = 0; i < n2; ++i){
+  let handx = 1000 + i * INTERVAL - Math.floor(Math.random() * (200 + 1));//障害物の右端指定
+  let handc = Math.floor(Math.random() * (550 + 1 - 200)) + 200;
+  let handCC = Math.floor(Math.random() * (130 + 1 - 100)) + 100;
+  let clearFlag = Boolean(false);
+  hands.push({handx,handc,handCC,clearFlag});
+}
+
+const foods = [];
+let n3 = 3;
+for (let i = 0; i < n3; ++i) {
+  let foodx = (hands[i*2+1].handx + hands[i*2].handx)/2;
+  let foody = Math.floor(Math.random() * (650 + 1 - 100)) + 100;//Math.floor(Math.random() * (550 + 1 - 200)) + 200;
+  foods.push({ foodx,foody });
+}
+
+let foodMeter = 5000;
+
+let touchHandFlag = false;
+let touchFoodFlag = false;
 
 let y_before = 0;
 
 const face = document.getElementById("face");
 const ctx = face.getContext('2d');
 
-// 定数
-const GRAVITY = 1.5; // 重力
-const JUMP_FORCE = -25; // ジャンプの力
-const FRAME_RATE = 60; // フレームレート
-
-function calc() {
+function calcPlayer() {
   player.velocity += GRAVITY;
   y_before = player.y;
   player.y += player.velocity;
-  for (let i = n-1; i >= 0; --i){
+  for (let i = n - 1; i >= 0; --i) {
     if (i == 0) {
       rects[i].yr = y_before;
     }
     else {
-      rects[i].yr = rects[i-1].yr
+      rects[i].yr = rects[i - 1].yr
     }
   }
 
-  for (let i = 0; i < n; ++i){
+  for (let i = 0; i < n; ++i) {
     if (i == 0) {
       dx = player.x - rects[i].xr;
       dy = player.y - rects[i].yr;
     }
     else {
-      dx = rects[i-1].xr - rects[i].xr;
-      dy = rects[i-1].yr - rects[i].yr;
+      dx = rects[i - 1].xr - rects[i].xr;
+      dy = rects[i - 1].yr - rects[i].yr;
     }
-    targetAngle = Math.atan2(dy, dx);
-    rects[i].angle = lerpAngle(rects[i].angle, targetAngle, 0.8);
+    rects[i].angle = Math.atan2(dy, dx);
+
     //console.log(rects[i].angle);
   }
 }
-function lerpAngle(a, b, t) {
-    const diff = b - a;
-    return a + diff * t;
+
+function calcHands() {
+  for (let i = 0; i < n2; ++i){
+    hands[i].handx -= MOVE_SPEED;
+    if (hands[i].handx < -60) {
+      if (i == 0) {
+        hands[0].handx = hands[5].handx + INTERVAL - Math.floor(Math.random() * (200 + 1));//障害物の右端指定
+      }
+      else {
+        hands[i].handx = hands[i-1].handx + INTERVAL - Math.floor(Math.random() * (200 + 1));//障害物の右端指定
+      }
+      hands[i].handc = Math.floor(Math.random() * (550 + 1 - 200)) + 200;
+      hands[i].handCC = Math.floor(Math.random() * (130 + 1 - 100)) + 100;
+      hands[i].clearFlag = false;
+    }
+  } 
+}
+
+function calcFood() {
+  for (let i = 0; i < n3; ++i) {
+    foods[i].foodx -= MOVE_SPEED;
+    if (foods[i].foodx < -400) {
+      foods[i].foodx = (hands[i * 2 + 1].handx + hands[i * 2].handx) / 2;//障害物の右端指定
+      foods[i].foody = Math.floor(Math.random() * (650 + 1 - 100)) + 100;
+    }
+  }
+}
+
+//必要なのはpx,py,r,ex,ey,w
+function touchCheck() {
+  let tempFlag = false;
+  hands.forEach(hand => {
+    if (tempFlag) return;
+    else if (player.x > hand.handx - PLAYER_R && player.x < hand.handx + HAND_SIZE + PLAYER_R) {
+      //プレイヤーのX座標が土管範囲内なら
+      if (player.x < hand.handx) {
+        let tempy = Math.acos((hand.handx-player.x)/PLAYER_R);
+        if (Math.sin(tempy) + player.y > hand.handc + hand.handCC || Math.sin(-tempy) + player.y < hand.handc - hand.handCC) {
+          //console.log("touch 1");
+          tempFlag = true;
+        }
+      }
+      else if (player.x > (hand.handx + HAND_SIZE)) {
+        let tempy = Math.acos((hand.handx + HAND_SIZE - player.x) / PLAYER_R);
+        if (Math.sin(tempy) + player.y > hand.handc + hand.handCC || Math.sin(-tempy) + player.y < hand.handc - hand.handCC) {
+          //console.log("touch 2");
+          tempFlag = true;
+        }
+      }
+      else {
+        if (player.y + PLAYER_R > hand.handc + hand.handCC || player.y - PLAYER_R < hand.handc - hand.handCC) {
+          //console.log("touch 3");
+          tempFlag = true;
+        }
+      }
+    }
+  })
+  return tempFlag; 
+}
+
+function touchFoodCheck() {
+  let tempFlag = false;
+  foods.forEach(food => {
+    if (tempFlag) return;
+    else if (player.x < food.foodx + 50 || player.x > food.foodx - 50) {
+      if (Math.pow(player.x - food.foodx, 2) + Math.pow(player.y - food.foody, 2) <= Math.pow(PLAYER_R + FOOD_R, 2)) {
+        tempFlag = true;
+        food.foody = 10000;
+      }
+    }
+  })
+  return tempFlag;
+}
+
+function calcMeter(flag) {
+  foodMeter--;
+  if (flag) {
+    foodMeter += 300;
+  }
+  //console.log(foodMeter);
+}
+
+function calcScore() {
+  hands.forEach(hand => {
+    if (hand.clearFlag == false && hand.handx < player.x - PLAYER_R - HAND_SIZE) {
+      hand.clearFlag = true;
+      score++;
+      console.log(score);
+    }
+  })
+}
+
+function calc() {
+  calcPlayer();
+  calcHands();
+  if (touchCheck()) {
+    gameFlag = false;
+    resultFlag = true;
+  }
+  calcScore();
+  calcFood();
+  touchFoodFlag = touchFoodCheck();
+  calcMeter(touchFoodFlag);
+  
 }
 
 
@@ -72,30 +213,96 @@ function drawRectangles() {
   });
 }
 
-function draw() {
 
+//100-130... 115から上下15
+function drawHands() {
+  hands.forEach(hand => {
+    ctx.beginPath();
+    //ctx.rect(100, 100, 100, 100);
+    ctx.rect(hand.handx, hand.handc + hand.handCC, HAND_SIZE, 750 - hand.handc-hand.handCC);
+    ctx.rect(hand.handx, 0, HAND_SIZE, hand.handc - hand.handCC);
+    
+    ctx.fillStyle = 'green'; // 例えば青色で塗りつぶす
+    ctx.fill();
+    ctx.stroke(); // 枠線を描画
+  })
+}
+
+function drawFoods() {
+  foods.forEach(food => {
+    ctx.beginPath();
+    ctx.arc(food.foodx, food.foody, FOOD_R, 0, Math.PI * 2, true);
+    ctx.fillStyle = 'yellow'; // 例えば青色で塗りつぶす
+    ctx.fill();
+    ctx.stroke(); // 枠線を描画
+  })
+}
+
+function draw() {
+  drawHands();
   drawRectangles();
+  drawFoods();
   ctx.beginPath();
   //ctx.arc(300, player.y, 50, 0, Math.PI * 2, true);
-  ctx.arc(player.x, player.y, 30, 0, Math.PI * 2, true);
+  ctx.arc(player.x, player.y, PLAYER_R, 0, Math.PI * 2, true);
   ctx.stroke();
+  if (touchHandFlag==true) {
+    ctx.beginPath();
+    ctx.rect(100, 100, 100, 100);
+    ctx.fillStyle = 'black'; // 例えば青色で塗りつぶす
+    ctx.fill();
+  }
+  
+}
+
+function drawResult() {
+  drawHands();
+  drawFoods();
+  document.getElementById('Tweet').style.display = 'block';
+  document.getElementById('oneMore').style.display = 'block';
+
 }
 
 // ジャンプ処理
 function jump() {
   player.velocity = JUMP_FORCE;
+  player.jumpFlag = 0;
+}
+
+
+function setting() {
+  document.getElementById('Tweet').style.display = 'none';
+  document.getElementById('oneMore').style.display = 'none';
+  //いんとろからゲームスタートのためのコードとか
+  //
+  //
+
 }
 
 // ゲームループ
 function gameLoop() {
 
-  ctx.clearRect(0, 0, 930,750);
-  calc();
-  draw();
+  ctx.clearRect(0, 0, 930, 750);
+  if (gameFlag) {
+    calc();
+    draw();
+  }
+  if (resultFlag) {
+    drawResult();
+  }
+  
 
   // 次のフレームを予約
   setTimeout(gameLoop, 1000 / FRAME_RATE);
 }
+
+document.getElementById('Tweet').addEventListener('click', function () {
+  //let text = document.getElementById("tweet-text").innerText;
+  // オプションパラメータを設定
+  var tweetText = `今回の獲得金額: ${window.scoreResult} \n＃AbilityMontage\nhttps://rusnrg1.github.io/Ability-Montage.github.io/`;
+  var tweetUrl = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(tweetText);
+  window.open(tweetUrl, '_blank');
+});
 
 // キーボードイベントのリスナーを設定
 document.addEventListener('keydown', function (event) {
@@ -104,6 +311,10 @@ document.addEventListener('keydown', function (event) {
   }
 });
 
+
+
+//初期配置(ボタンを指定場所において非表示にしたりする。読み込み自体はinitでやる)
+setting();
 // ゲーム開始
 gameLoop();
 
--- FlappyTotetsu/init.js
+++ FlappyTotetsu/init.js
@@ -11,15 +11,15 @@ sceneElements = document.querySelectorAll('.scene');
 
 // ここで showLoadingScreen, preloadAllResources, hideLoadingScreen, showTitleScreen 関数を定義
 function parseCSV(text) {
-    const lines = text.split('\n');
-    const headers = lines[0].split(',');
-    return lines.slice(1).map(line => {
-      const data = line.split(',');
-      return headers.reduce((obj, nextKey, index) => {
-        obj[nextKey] = data[index];
-        return obj;
-      }, {});
-    });
+  const lines = text.split('\n');
+  const headers = lines[0].split(',');
+  return lines.slice(1).map(line => {
+    const data = line.split(',');
+    return headers.reduce((obj, nextKey, index) => {
+      obj[nextKey] = data[index];
+      return obj;
+    }, {});
+  });
 }
 
 
@@ -30,46 +30,46 @@ function animateUFO() {
   const ufo1 = document.getElementById('ufo1');
   const ufo2 = document.getElementById('ufo2');
   const ufo3 = document.getElementById('ufo3');
-  
+
   const radius = 100;
   let centerX = 465;
   let centerY = 450;
   let angle1 = 0;
-  let angle2 = 120*Math.PI/180;
-  let angle3 = 240*Math.PI/180;
+  let angle2 = 120 * Math.PI / 180;
+  let angle3 = 240 * Math.PI / 180;
 
   function update() {
-      // 角度を更新
-      angle1 += 0.04; // 速度調整
-      angle2 += 0.04; // 速度調整
-      angle3 += 0.04; // 速度調整
+    // 角度を更新
+    angle1 += 0.04; // 速度調整
+    angle2 += 0.04; // 速度調整
+    angle3 += 0.04; // 速度調整
 
-      // 新しい位置を計算
-      const x1 = centerX + radius * Math.cos(angle1);
-      const y1 = centerY + radius * Math.sin(angle1);
+    // 新しい位置を計算
+    const x1 = centerX + radius * Math.cos(angle1);
+    const y1 = centerY + radius * Math.sin(angle1);
 
-      // UFOの位置を更新
-      ufo1.style.left = `${x1*scale}px`;
+    // UFOの位置を更新
+    ufo1.style.left = `${x1 * scale}px`;
     ufo1.style.top = `${y1 * scale}px`;
 
-      const x2 = centerX + radius * Math.cos(angle2);
-      const y2 = centerY + radius * Math.sin(angle2);
+    const x2 = centerX + radius * Math.cos(angle2);
+    const y2 = centerY + radius * Math.sin(angle2);
 
-      // UFOの位置を更新
+    // UFOの位置を更新
     ufo2.style.left = `${x2 * scale}px`;
     ufo2.style.top = `${y2 * scale}px`;
 
-      const x3 = centerX + radius * Math.cos(angle3);
-      const y3 = centerY + radius * Math.sin(angle3);
+    const x3 = centerX + radius * Math.cos(angle3);
+    const y3 = centerY + radius * Math.sin(angle3);
 
-      // UFOの位置を更新
+    // UFOの位置を更新
     ufo3.style.left = `${x3 * scale}px`;
     ufo3.style.top = `${y3 * scale}px`;
 
 
 
-      // 次のフレームのために自身を呼び出す
-      requestAnimationFrame(update);
+    // 次のフレームのために自身を呼び出す
+    requestAnimationFrame(update);
   }
 
   update();
@@ -77,82 +77,82 @@ function animateUFO() {
 
 
 function showLoadingScreen() {
-    document.getElementById('loadingScreen').style.display = 'block';
-    animateUFO();
+  //document.getElementById('loadingScreen').style.display = 'block';
+  //animateUFO();
 
 }
 
 function hideLoadingScreen() {
-  document.getElementById('loadingScreen').style.display = 'none';
+  //document.getElementById('loadingScreen').style.display = 'none';
 }
 
 function preloadAllResources(callback) {
-    preloadImages(() => {
-      preloadTexts(() => {
-        preloadSound(() => {
-          callback();  
-        })
-      })
-    })
+  //preloadImages(() => {
+    //preloadTexts(() => {
+      //preloadSound(() => {
+        //callback();
+      //})
+    //})
+  //})
 }
 
 function preloadImages(callback) {
-    fetch("buttons.csv")
-      .then(response => response.text())
-      .then(text => {
-        const buttons = parseCSV(text);
-        buttons.forEach(buttonData => {
-          const buttonimg = new Image();
-          buttonimg.classList.add('imageButton');
-          buttonimg.src = buttonData.src;
-          buttonimg.folder = buttonData.folder;
-          buttonimg.alt = buttonData.alt;
-          buttonimg.setAttribute('data-folder', buttonimg.folder);
-          window.imageElements.push(buttonimg);
-          let map = new Map();
-          for (let i = 0; i < buttonimg.alt.length; i++) {
-            const gameimg = new Image();
-            gameimg.src = `images/${buttonimg.folder}/${i.toString().padStart(2, '0')}.png`;
-            gameimg.alt = buttonimg.alt[i];
-            map.set(gameimg.alt,gameimg.src);
-          }
-          window.gameImageElements.push(map);  
-        });
-        callback();
-      })
-      .catch(error => console.error("CSVの読み込みに失敗", error));
+  fetch("buttons.csv")
+    .then(response => response.text())
+    .then(text => {
+      const buttons = parseCSV(text);
+      buttons.forEach(buttonData => {
+        const buttonimg = new Image();
+        buttonimg.classList.add('imageButton');
+        buttonimg.src = buttonData.src;
+        buttonimg.folder = buttonData.folder;
+        buttonimg.alt = buttonData.alt;
+        buttonimg.setAttribute('data-folder', buttonimg.folder);
+        window.imageElements.push(buttonimg);
+        let map = new Map();
+        for (let i = 0; i < buttonimg.alt.length; i++) {
+          const gameimg = new Image();
+          gameimg.src = `images/${buttonimg.folder}/${i.toString().padStart(2, '0')}.png`;
+          gameimg.alt = buttonimg.alt[i];
+          map.set(gameimg.alt, gameimg.src);
+        }
+        window.gameImageElements.push(map);
+      });
+      callback();
+    })
+    .catch(error => console.error("CSVの読み込みに失敗", error));
 }
 
 
-function preloadGameImages(){
+function preloadGameImages() {
   //alert(window.imageElements[0]);
-  imageElements.forEach(buttonImage =>{
+  imageElements.forEach(buttonImage => {
     let map = new Map();
-    
+
     for (let i = 0; i < buttonImage.alt.length; i++) {
       const img = new Image();
       img.src = `images/${buttonImage.folder}/${i.toString().padStart(2, '0')}.png`;
       img.alt = buttonImage.alt[i];
-      map.set(img.alt,img.src);
+      map.set(img.alt, img.src);
     }
-    gameImage.push(map);    
+    gameImage.push(map);
   })
 }
 
 function preloadTexts(callback) {
-    fetch("text_Koryudo.csv")
-      .then(response => response.text())
-      .then(data => {
-        const text = parseCSV(data);
-        text.forEach(t => {
-          window.textdata.push(t.text.trim());
-          window.textLevel.push(t.level);
-          window.textFlag.push(!!Number(t.flag));
-          window.textMax.push(t.max);
-        });
-      })  
-      .catch(error => console.error("CSVの読み込みに失敗", error));
-    callback();
+  fetch("text_Koryudo.csv")
+    .then(response => response.text())
+    .then(data => {
+      const text = parseCSV(data);
+      text.forEach(t => {
+        window.textdata.push(t.text.trim());
+        window.textLevel.push(t.level);
+        window.textFlag.push(!!Number(t.flag));
+        window.textMax.push(t.max);
+      });
+    })
+    .catch(error => console.error("CSVの読み込みに失敗", error));
+  callback();
 }
 
 function preloadSound(callback) {
@@ -165,22 +165,22 @@ function preloadSound(callback) {
   document.getElementById("BGM").src = soundsToLoad[5];
   document.getElementById("gameClearBGM").src = soundsToLoad[6];
   callback();
-  }
+}
 
 //各フォルダとファイル名オブジェクトを作る関数
 function generateImageNames(numberOfImages) {
   // 画像ファイル名の配列を生成
   let imageNames = [];
   for (let i = 0; i < numberOfImages; i++) {
-      // ゼロ埋めした画像ファイル名を生成（例：'00.gif', '01.gif', ...）
-      let imageName = `${i.toString().padStart(2, '0')}.png`;
-      imageNames.push(imageName);
+    // ゼロ埋めした画像ファイル名を生成（例：'00.gif', '01.gif', ...）
+    let imageName = `${i.toString().padStart(2, '0')}.png`;
+    imageNames.push(imageName);
   }
   // 結果をオブジェクトとして返す
   return imageNames;
 }
 
-function loadImageFromFolder(folderName, imageNum,indexNum) {
+function loadImageFromFolder(folderName, imageNum, indexNum) {
   const gameImages = document.getElementById('gameImages');
   const images = generateImageNames(imageNum)
   const img = document.createElement('img');
@@ -211,6 +211,26 @@ function displayImage() {
 }
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    changeScene("gameScene");
+document.addEventListener('DOMContentLoaded', function () {
+  showLoadingScreen();
+
+  //var backimg = document.querySelector('.backImage');
+  //backimg.style.backgroundImage = 'url(images/Back.jpg)';
+
+  const onemoreimg = document.createElement('img');
+  onemoreimg.src = `images/OneMore.png`;
+  document.getElementById("oneMore").appendChild(onemoreimg);
+
+  const tweetimg = document.createElement('img');
+  tweetimg.src = `images/Tweet.png`;
+  document.getElementById("Tweet").appendChild(tweetimg);
+  changeScene("gameScene");
+  //preloadAllResources(() => {
+    //setTimeout(() => {
+      //displayImage();
+      //hideLoadingScreen();
+      //document.getElementById("titleImage").style.display = "block";
+      
+    //}, 3000);
+  //});
 });
